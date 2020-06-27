b392ad20df018877a747df1664dae5a76244d507
Eva Navarro <emanao@me.com>
2020-06-27 19:36:52 +0000

    Challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", DogCEOFetch)
+function DogCEOFetch(){
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(response => response.json())
+  .then((json) => {
+    const dogContainer = document.getElementById("dog-image-container");
+    const dogsAry = json.message;
+    for (const dog of dogsAry){
+      const img = document.createElement("img");
+      img.setAttribute("src", dog);
+      img.setAttribute("width", 200);
+      img.setAttribute("height", 200);
+      dogContainer.appendChild(img);
+    }
+  })
+}
