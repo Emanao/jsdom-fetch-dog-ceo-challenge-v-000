bb327afba4be59249d8e6a9ba716c551dbe18109
Eva Navarro <emanao@me.com>
2020-06-27 20:04:29 +0000

    challenge 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 console.log('%c HI', 'color: firebrick')
-document.addEventListener("DOMContentLoaded", DogCEOFetch)
-function DogCEOFetch(){
-  fetch("https://dog.ceo/api/breeds/image/random/4")
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+document.addEventListener("DOMContentLoaded", DogCEOPicsFetch);
+document.addEventListener("DOMContentLoaded", DogCEOBreedsFetch);
+function DogCEOPicsFetch(){
+  fetch(imgUrl)
   .then(response => response.json())
   .then((json) => {
     const dogContainer = document.getElementById("dog-image-container");
@@ -12,6 +15,26 @@ function DogCEOFetch(){
       img.setAttribute("width", 200);
       img.setAttribute("height", 200);
       dogContainer.appendChild(img);
+    }
+  });
+}
+function DogCEOBreedsFetch(){
+  fetch(breedUrl)
+  .then(response => response.json())
+  .then((json) => {
+    console.log(json);
+     const breedsContainer = document.getElementById("dog-breeds");
+     const typesAry = json.message;
+     console.log(typesAry);
+     for (const type in typesAry){
+       const breedsAry= typesAry[type];
+       if (Array.isArray(breedsAry)){
+         for (const breed of breedsAry){
+           const li = document.createElement("li");
+           li.innerText = breed;
+           breedsContainer.appendChild(li);
+         }
+       }
     }
   })
 }
